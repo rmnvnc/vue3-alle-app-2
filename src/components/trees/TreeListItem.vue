40099d6d6f79398f9b9418bae685887b8c6bcf94
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2025-06-21 10:01:08 +0200

    Trees list - color difference by wateredUntil

--- src/components/trees/TreeListItem.vue
+++ src/components/trees/TreeListItem.vue
@@ -1,13 +1,11 @@
 <template>
-    <li>
+    <li :class="waterStatus">
         <router-link v-if="tree.slug && tree.id" :to="{
             name: 'tree-view',
             params: { orgId: orgId, orchardId: orchardId, treeId: tree.id, treeSlug: tree.slug }
         }">{{ tree.name }}
             <br>
             {{ remainingText }}
-
-            {{ remainingDays }}
             <template v-if="tree.owner">
                 <br>
                 Majitel: {{ tree.owner }}
@@ -39,13 +37,53 @@ const { remainingText, remainingDays } = useRemainingTime(
     computed(() => tree?.wateredUntil ?? null)
 )
 
+const waterStatus = computed(() => {
+    let status = ''
+    if (remainingDays.value > 12) {
+        status = 'green';
+    } else if (remainingDays.value > 7) {
+        status = 'lightGreen'
+    } else if (remainingDays.value > 1) {
+        status = 'yellow'
+    } else if (remainingDays.value > - 2) {
+        status = 'orange'
+    } else {
+        status = 'red'
+    }
+    return status;
+})
+
 </script>
 
 <style scoped>
 li {
     padding: 16px;
     border: 1px solid var(--vt-c-text-dark-2);
-    ;
     margin-bottom: 8px;
+}
+
+li.red {
+    border-color: red;
+    background-color: rgba(255, 0, 0, 0.1);
+}
+
+li.yellow {
+    border-color: yellow;
+    background-color: rgba(255, 255, 0, 0.1)
+}
+
+li.orange {
+    border-color: orange;
+    background-color: rgba(255, 166, 0, 0.1);
+}
+
+li.green {
+    border-color: green;
+    background-color: rgba(0, 128, 0, 0.1);
+}
+
+li.greenLight {
+    border-color: rgb(0, 225, 0);
+    background-color: rgba(0, 225, 0, 0.1);
 }
 </style>
