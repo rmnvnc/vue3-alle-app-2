b124aa37aea947392db20e8fb75e39750019a31e
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2025-07-02 21:08:22 +0200

    auth - roles

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,9 +1,14 @@
 <template>
     <header>
-        The Header
-        <span v-if="auth.isLoggedIn">
-            <br>Vitaj, {{ auth.user.email }} <base-button @click="auth.logout()">Odhlásiť</base-button>
-        </span>
+        <template v-if="auth.isLoggedIn">
+            Vitaj, {{ auth.user.email }} <base-button @click="auth.logout()">Odhlásiť</base-button>
+            <span class="role-check">
+                <span class="active">Sledovať</span><br>
+                <span :class="{ active: auth.canWater }">Polievať</span><br>
+                <span :class="{ active: auth.canEdit }">Editovať</span><br>
+                <span :class="{ active: auth.isAdmin }">Admin</span>
+            </span>
+        </template>
     </header>
 </template>
 
@@ -13,4 +18,15 @@ import { useAuthStore } from '@/stores/auth.js'
 const auth = useAuthStore()
 </script>
 
-<style scoped></style>
+<style scoped>
+.role-check span:not(.active) {
+    opacity: .6;
+}
+
+header {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    justify-content: space-between;
+}
+</style>
